d6133e18331802430f9ed3bb3d28777928c3887a
IulianMohonea <126501200+IulianMohonea@users.noreply.github.com>
2024-06-13 20:13:02 +0300

    Add files via upload

--- player.gd
+++ player.gd
@@ -1,17 +1,35 @@
 extends CharacterBody2D
 const bulletPath = preload('res://Bullet.tscn')
+@onready var pause_menu = $PauseMenu
 
 var score = 0
+var collect = 0
 var _animated_sprite = 1
 
 var MAX_HP = 5
 var hp = MAX_HP
 
+var paused = true;
+
+
 func _ready():
 	_animated_sprite = $AnimatedSprite2D
+	pause_menu = $PauseMenu
 	set_hp()
+	pauseMenu()
 	
-	
+
+func pauseMenu():
+	print("Paused ")
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+		
+	paused = !paused
 	
 ## Setez hp-ul
 func set_hp():
@@ -25,7 +43,7 @@ func name():
 	
 ## Setez label-ul la hp (e cam mare, ma mai gandesc daca sa il folosesc sau nu)
 func set_hp_label():
-	$HealthLabel.text =  "Health: "
+	$HealthLabel.text =  "A"
 	
 ## Setez bara de hp(se updateaza hp ul actual) 
 func set_hp_bar():
@@ -43,11 +61,15 @@ func _process(_delta):
 	
 	 ## $TimeRemaining.text = "%s" % roundf($BulletTimer.time_left) -- ref for spells timers
 	_animated_sprite.play("Idle")
+	#pause_menu.hide()
+	if paused == false:
+		if Input.is_action_just_pressed("shoot") and $BulletTimer.get_time_left() == 0:
+			shoot()
+			$BulletTimer.start()
+		$Node2D.look_at(get_global_mouse_position())
 	
-	if Input.is_action_just_pressed("shoot") and $BulletTimer.get_time_left() == 0:
-		shoot()
-		$BulletTimer.start()
-	$Node2D.look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("pause"):
+		pauseMenu()
 
 
 func kill():
@@ -71,25 +93,23 @@ func _physics_process(_delta):
 	
 	##if is_on_floor()
 	
-	
-	if Input.is_action_pressed("move_right"):
-		position.x += max_speed 
-		$AnimatedSprite2D.flip_h = false
-	if Input.is_action_pressed("move_left"):
-		position.x -= max_speed
-		$AnimatedSprite2D.flip_h = true
-	if Input.is_action_pressed("move_down"):
-		position.y += max_speed
-		$AnimatedSprite2D.flip_h = false 
-	if Input.is_action_pressed("move_up"):
-		position.y -= max_speed
-		$AnimatedSprite2D.flip_h = false
-	else:
+	if paused == false:
+		if Input.is_action_pressed("move_right"):
+			position.x += max_speed 
+			$AnimatedSprite2D.flip_h = false
+		if Input.is_action_pressed("move_left"):
+			position.x -= max_speed
+			$AnimatedSprite2D.flip_h = true
+		if Input.is_action_pressed("move_down"):
+			position.y += max_speed
+			$AnimatedSprite2D.flip_h = false 
+		if Input.is_action_pressed("move_up"):
+			position.y -= max_speed
+			$AnimatedSprite2D.flip_h = false
+		else:
+			position.x += 0
+		
 		position.x += 0
+		move_and_slide()
 	
-	position.x += 0
-	move_and_slide()
-	if Input.is_action_just_released("ui_select"):
-		if velocity.y < 0:
-			velocity.y += 400
 	

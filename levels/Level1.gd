0f6c807bdd9d4e5746c8bc3f9bc8ce7525345659
Theodor Negrescu <theodor.negrescu@gmail.com>
2024-06-14 15:52:42 +0300

    Add background music

--- levels/Level1.gd
+++ levels/Level1.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 var game_over_next_scene = "res://menu.tscn"
 
+var _level_music = preload("res://Art/2019-12-11_-_Retro_Platforming_-_David_Fesliyan.mp3")
+var _player
+
 @onready var minimap = $Minimap
 @onready var tilemap = $TileMap
 @onready var player = $Player
@@ -10,6 +13,10 @@ var game_over_next_scene = "res://menu.tscn"
 func _ready():
 	Persistence.register_scene_to_autosave(self)
 	minimap.add_tilemap(tilemap)
+	_player = AudioStreamPlayer.new()
+	_player.stream = _level_music
+	add_child(_player)
+	_player.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
